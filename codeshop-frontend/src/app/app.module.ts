4d19fc0c97d881ee8e76c129f79df3a36bf1e912
vipin <vipin@Nousheens-iMac.local>
2020-03-26 20:54:37 +0000

    header, home, routing and partial store page

--- codeshop-frontend/src/app/app.module.ts
+++ codeshop-frontend/src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HomeComponent } from './home/home.component';
+import { StoreComponent } from './store/store.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'store', component: StoreComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CarouselComponent,
+    HomeComponent,
+    StoreComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true}
+    ),
     BrowserModule,
     AppRoutingModule,
     NgbModule
