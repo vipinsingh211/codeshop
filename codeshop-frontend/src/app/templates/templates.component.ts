0d78214086f5689545cfdbb3b44506211c082afb
vipin <vipin@Nousheens-iMac.local>
2020-04-09 20:52:18 +0100

    template page implementation

--- codeshop-frontend/src/app/templates/templates.component.ts
+++ codeshop-frontend/src/app/templates/templates.component.ts
@@ -7,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplatesComponent implements OnInit {
 
-  constructor() { }
+  public categories = { };
+  public categoryTypes: string[];
+
+  constructor() {
+    this.categories = {
+      Search: [
+        {
+          name: 'Classic Search',
+          previewImage: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.stack.imgur.com%2F7dbMR.jpg&f=1&nofb=1',
+          price: '£ 12'
+        },
+        {
+          name: 'Business Search',
+          previewImage: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.stack.imgur.com%2F7dbMR.jpg&f=1&nofb=1',
+          price: '£ 12'
+        }
+      ],
+      Header: [
+        {
+          name: 'Header 1',
+          previewImage: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.stack.imgur.com%2F7dbMR.jpg&f=1&nofb=1',
+          price: '£ 12'
+        }
+      ]
+    };
+
+    this.categoryTypes = Object.keys(this.categories);
+  }
 
   ngOnInit(): void {
   }
